fece82c3a43dcea091e20e4b8acdb5cf38408354
ParthaMalakar <102042225+ParthaMalakar@users.noreply.github.com>
2022-06-13 13:26:56 +0600

    lab -5 task done

---
+++ check.php
@@ -0,0 +1,26 @@
+<?php 
+	session_start();
+
+
+
+	$name = $_REQUEST['name'];
+	$id = $_REQUEST['id'];
+	$email = $_REQUEST['email'];
+	$date = $_REQUEST['date'];
+	$password = $_REQUEST['password'];
+	
+
+	if ($name == null || $id == null || $email==null || $password ==null ) {
+		echo "invalid type everythings<br>";
+	}else{
+		if($name == $password){
+			$_SESSION['status'] = true;
+			header('location: home.php');
+		}else{
+			echo "invalid user";
+		}
+	}
+
+
+
+?> 
---
+++ registration.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Create a new account</title>
+</head>
+<body>
+		<form action="Check.php" method="post" enctype="">
+			<fieldset>
+				<legend>Create a new account</legend>
+				<table>
+					<tr>
+						<td>Name</td>
+						<td><input type="text" name="name" value=""></td>
+					</tr>
+					<tr>
+						<td>National id</td>
+						<td><input type="text" name="id" value=""></td>
+					</tr>
+					<tr>
+						<td>Email</td>
+						<td><input type="email" name="email" value=""></td>
+					</tr>
+										<tr>
+						<td>DOB</td>
+						<td><input type="date" name="date" value=""></td>
+					</tr>
+					<tr>
+						<td>Password</td>
+						<td><input type="password" name="password" value=""></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" name="" value="Resister"></td>
+					</tr>
+				</table>
+			</fieldset>
+		</form>
+</body>
+</html>
