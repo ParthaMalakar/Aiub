aa624e0958d663c1c52ff66877ce36a318c967b3
ParthaMalakar <102042225+ParthaMalakar@users.noreply.github.com>
2022-06-20 13:31:02 +0600

    lab performance done

--- ADMINHOME.php
+++ ADMINHOME.php
@@ -3,7 +3,7 @@
 <body>
 <h1 style="align:center">WELCOME Bob?</h1><br>
 <a href="PROFILE.php"> PROFILE </a><br>
-<a href="PASSWORDCHANGE.php"> CHANGE PASSWORD </a><br>
+<a href="changepassword.php"> CHANGE PASSWORD </a><br>
 <a href="viewuser.php"> View user </a><br>
 <a href="logout.php">LOGOUT</a>
 
