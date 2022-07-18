26191c9ddb0bdd91620df7147b7196fa5c6abe5f
ParthaMalakar <102042225+ParthaMalakar@users.noreply.github.com>
2022-07-18 14:40:44 +0600

    done

--- Final_Lab_Task1/index.php
+++ Final_Lab_Task1/index.php
@@ -1,1 +1,40 @@
+<?php 
 
+    $conn = mysqli_connect('localhost', 'root', '', 'webtech');
+
+    // if($conn){
+    //     echo "Success";
+    // }else{
+    //     echo "DB Error...";
+    // }
+
+    $sql = "select * from users";
+    $result = mysqli_query($conn, $sql);
+    
+  echo  "<table border ='2'>";
+  echo   "<tr>";
+  echo   "<th>Id</th>";
+  echo   "<th>Name</th>";
+  echo "<th>Password</th>";
+  echo "<th>Email</th>";
+  echo  "</tr>";
+ 
+
+
+
+while($row= mysqli_fetch_assoc($result)){
+      echo "<tr><td>";
+      echo $row['Id']."</td>";
+      echo "<td>".$row['Name']."</td>";
+      echo "<td>". $row['Password'] ."</td>";
+       echo "<td>". $row['Email']."</td>";
+
+
+    
+
+     
+       
+    }
+
+     echo "</table>";
+?>
